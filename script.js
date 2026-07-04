7328b09c7476f768f7bba13ef08ccb0e9f7bad56
Kurtić Džemal <Dzemaludeen@gmail.com>
2026-07-04 20:56:37 +0200

    Finish the JS file

--- script.js
+++ script.js
@@ -1,140 +1,84 @@
-const main = document.querySelector("main");
+const main = document.querySelector(".main");
+const addBookBtn = document.querySelector("#addBook")
+const bookIsRead = document.querySelector("#check")
+const form = document.querySelector("#form")
+form.addEventListener("submit", createBook)
 
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID()
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.info = function () {
+        return `${this.title}, by ${this.author}, ${this.pages}, isRead = ${this.isRead}.`;
+    }
 }
 
-Book.prototype.info = function () {
-    return `${this.title}, by ${this.author}, ${this.pages}, isRead = ${this.isRead}.`;
+function createBook(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formValues = Object.fromEntries(formData);
+
+    book = new Book(formValues.name, formValues.author, formValues.pages, bookIsRead.checked)
+    addBookToLibrary(book)
+
+    this.reset();
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    main.replaceChildren();
+
+    showBooks()
 }
 
 function showBooks() {
     for (i = 0; i < myLibrary.length; i++) {
         book = myLibrary[i];
-        createBookCard();
+        createBookCard(book);
     }
 }
 
-function createBookCard(){
+function createBookCard(book) {
     const bookCard = document.createElement("div");
-        bookCard.style.border = "3px solid blue";
-        main.appendChild(bookCard);
+    bookCard.style.border = "3px solid black";
+    bookCard.style.backgroundColor = "lightgreen";
+
+    const bookTitle = document.createElement("div");
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("div");
+    bookAuthor.textContent = book.author;
+    bookCard.appendChild(bookAuthor);
+
+    const bookPages = document.createElement("div");
+    bookPages.textContent = `${book.pages} pages`;
+    bookCard.appendChild(bookPages);
+
+    const bookIsRead = document.createElement("div");
+    bookIsRead.textContent = book.isRead;
+    bookCard.appendChild(bookIsRead);
+
+    const button = document.createElement("button");
+    button.innerText = "Delete";
+    button.setAttribute("id", `${book.id}`);
+    button.setAttribute("class", `deleteBtn`);
+
+    button.addEventListener("click", () => {
+        myLibrary = myLibrary.filter(b => b.id != book.id);
+        main.replaceChildren();
+        showBooks()
+    })
+
+    bookCard.appendChild(button)
+    main.appendChild(bookCard);
 }
 
-// Just for now
 
-let book1 = Object.create(Book.prototype);
-book1.title = "The Hobbit";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-
-let book2 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book3 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book4 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book5 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book6 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book7 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book8 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book9 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book10 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book11 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book12 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book13 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book14 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-let book15 = Object.create(Book.prototype);
-book1.title = "The Hobbit2";
-book1.author = "JRR";
-book1.pages = 233;
-book1.isRead = true;
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-// console.log(myLibrary);
-addBookToLibrary(book3);
-addBookToLibrary(book4);
-// console.log(myLibrary);
-addBookToLibrary(book5);
-addBookToLibrary(book6);
-// console.log(myLibrary);
-addBookToLibrary(book7);
-addBookToLibrary(book8);
-// console.log(myLibrary);
-addBookToLibrary(book9);
-addBookToLibrary(book10);
-// console.log(myLibrary);
-addBookToLibrary(book11);
-addBookToLibrary(book12);
-// console.log(myLibrary);
-addBookToLibrary(book13);
-addBookToLibrary(book14);
-// console.log(myLibrary);
-addBookToLibrary(book15);
-addBookToLibrary(book8);
-// console.log(myLibrary);
-
-
-showBooks();
 
 
